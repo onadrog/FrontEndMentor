897f584e30cea92a906838800b99f821fd777aea
onadrog <sebastien.gordano@gmail.com>
2024-08-19 13:59:39 +0200

    Fix: Scss reset / Feat: css stylesheet

--- starter.sh
+++ starter.sh
@@ -1,10 +1,19 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 case "$2" in
-	--nots)
+	--scss)
 		mkdir $1/fonts && \
 			cp lazy/style.scss $1/
 		;;
+    --css)
+        cp lazy/style.css $1/
+        ;;
+    --css-ts)
+        mkdir $1/src && \
+            touch $1/src/app.ts && \
+            cp lazy/tsconfig.json $1 && \
+            cp lazy/style.css $1
+        ;;
 	*)
 		mkdir $1/fonts && \
 			cp lazy/style.scss $1/ && \
